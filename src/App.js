98c69f1a3fc333a644c79eddfedf6baf08eaa27d
=Lil'Sasah <=sambatriniaina.18@gmail.com>
2024-07-19 17:29:42 +0300

    BACKGROUNDIMAGE j'ai agrandisser l'image de fond

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import backg from "./background.jpg"
+import backg from "./background.jpg";
 import './App.css';
 
 function App() {
   return (
     <main className="container mx-auto p-4">
-    <section
-      className="my-8 bg-cover bg-center relative"
-      style={{ backgroundImage: `url(${backg})`, height: '300px' }}
-    >
-      <div className="absolute inset-0 bg-black opacity-50"></div>
-      <div className="relative z-10 text-white p-8">
-        <h1 className="text-4xl font-bold mb-4">Les Tendances en Design Web Qui Captivent Vos Visiteurs</h1>
-       
-      </div>
-    </section>
-
-      
+      <section
+        className="my-8 bg-cover bg-center relative h-[400px] lg:h-[600px]"
+        style={{ backgroundImage: `url(${backg})` }}
+      >
+        <div className="absolute inset-0 bg-black bg-opacity-30"></div>
+        <div className="relative z-10 text-white p-8 flex flex-col items-center justify h-full">
+          <h1 className="text-4xl font-bold mb-4 text-center">Les Tendances en Design Web Qui Captivent Vos Visiteurs</h1>
+    
+        </div>
+      </section>
     </main>
   );
 }
